078502c45f03468aa84f8675b19fc247240e47ce
QZR <me@qzr.here>
2020-09-27 18:08:53 -0400

    Changes: - Cleaned up class 'cMain' constructor (1st pass)
    Issues:
    - Seems like a little lag when the initial welcome dialog is closed..

--- cMain.cpp
+++ cMain.cpp
@@ -9,21 +9,20 @@
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxPoint(100, 100), wxSize(600, 665))
 {
-	//##//Setting size hints
+	//Setting size hints
 	this->SetSizeHints(wxSize(250,250), wxSize(1000,1000));
 
 	btn = new wxBitmapButton*[nFieldWidth*nFieldHeight];
 	wxGridSizer *grid = new wxGridSizer(nFieldWidth, nFieldHeight, 0, 0);
-
 	nField = new int[nFieldWidth * nFieldHeight];
 
-	//##//Indicates presence of mine flags over buttons
+	//Create array indicating presence of mine flags over buttons
 	hasMineFlag = new bool[nFieldWidth * nFieldHeight];
 	for (int x = 0; x < nFieldWidth; x++)
 		for (int y =0; y <nFieldHeight; y++)
 		{ hasMineFlag[y*nFieldWidth + x] = false; }
 
-	//##//Adding toolbar and buttons for game control
+	//Adding toolbar and buttons for game control
 	wxToolBar *toolbar1 = this->CreateToolBar(wxTB_HORIZONTAL, wxID_ANY);
 	wxBitmapButton *settingsButton = new wxBitmapButton(toolbar1, 9001, *bmpSettings40p, wxDefaultPosition, wxSize(40,40), 0);
 	wxBitmapButton *forfeitButton = new wxBitmapButton(toolbar1, 9002, *bmpForfeit40p, wxDefaultPosition, wxSize(40,40), 0);
@@ -45,18 +44,15 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxP
 	//End of toolbar section
 
 
-	wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
-
 	for (int x = 0; x < nFieldWidth; x++)
 	{
 		for (int y =0; y <nFieldHeight; y++)
 		{
 			btn[y*nFieldWidth + x] = new wxBitmapButton(this, 10000 + (y*nFieldWidth +x), *bmpBlank, wxDefaultPosition,wxSize(10,10));
-			//btn[y*nFieldWidth + x]->SetFont(font);//Font is useless after switch to wxBitmapButton
 			grid->Add(btn[y*nFieldWidth+x], 1, wxEXPAND | wxALL);
 
-			btn[y*nFieldWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
-			btn[y*nFieldWidth + x]->Bind(wxEVT_RIGHT_DOWN, &cMain::OnRightClick, this); //!!!// Binding right click event to buttons
+			btn[y*nFieldWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this); //Binding left click events to buttons
+			btn[y*nFieldWidth + x]->Bind(wxEVT_RIGHT_DOWN, &cMain::OnRightClick, this); //Binding right click event to buttons
 			nField[y*nFieldWidth + x] = 0;
 		}
 	}	
@@ -64,52 +60,52 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxP
 	//##//Setting sizer within sizer to attempt to control aspect ratio
 	wxBoxSizer *vBox = new wxBoxSizer(wxVERTICAL);
 	vBox->Add(grid, 1, wxSHAPED | wxALIGN_CENTER);
-	this->SetSizer(vBox); //##//Changed parameter from 'grid' to vBox
-	grid->Layout(); //##//See above
+	this->SetSizer(vBox); //Changed parameter from 'grid' to vBox
+	grid->Layout(); //See above
 	vBox->Layout();
 
 	//##//implementing a status bar
 	statsbar1 = this->CreateStatusBar(1, wxSTB_DEFAULT_STYLE, wxID_ANY);
-	//SECTION FOR UPDATE
-	wxBoxSizer *statsSizer = new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer *hbox1_StatsB = new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer *hbox2_StatsB = new wxBoxSizer(wxHORIZONTAL);
+	
 	wxStaticText *statsbarTxt1 = new wxStaticText(statsbar1, wxID_ANY, wxString("Flags Remaining: "), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER_HORIZONTAL);
-	//statsbarTxt1->SetBackgroundColour(*wxGREEN);
 	statsbarTxt2 = new wxStaticText(statsbar1, wxID_ANY, wxString(" x ")<<std::to_string(lvlSelect), wxDefaultPosition, wxDefaultSize);
-	//statsbarTxt2->SetBackgroundColour(*wxGREEN);
 	statsbarTxt2->SetForegroundColour(*wxRED);
 	statsbarTxt2->SetFont(statsbarTxt2->GetFont().MakeBold());
 	wxStaticText *statsbarTxt3 = new wxStaticText(statsbar1, wxID_ANY, wxString("Current Score: "), wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
-	//statsbarTxt3->SetBackgroundColour(*wxYELLOW);
 	statsbarTxt4 = new wxStaticText(statsbar1, wxID_ANY, wxString("0"), wxDefaultPosition, wxDefaultSize);
-	//statsbarTxt4->SetBackgroundColour(*wxYELLOW);
 	statsbarTxt4->SetForegroundColour(*wxBLUE);
 	statsbarTxt4->SetFont(statsbarTxt4->GetFont().MakeBold());
 	wxStaticBitmap *statsbarBMP = new wxStaticBitmap(statsbar1, wxID_ANY, *bmpFlag20p, wxDefaultPosition, wxSize(20, 20));//Working as should
-	//hbox1_StatsB->AddStretchSpacer(1);
+	
+	wxBoxSizer *statsSizer = new wxBoxSizer(wxHORIZONTAL);
+	wxBoxSizer *hbox1_StatsB = new wxBoxSizer(wxHORIZONTAL);
+	wxBoxSizer *hbox2_StatsB = new wxBoxSizer(wxHORIZONTAL);
 	hbox1_StatsB->Add(statsbarTxt1, 1, wxALIGN_CENTER | wxLEFT, 45);
 	hbox1_StatsB->Add(statsbarBMP, 0, wxALIGN_CENTER);
 	hbox1_StatsB->Add(statsbarTxt2, 1, wxALIGN_CENTER);
-	//hbox1_StatsB->AddStretchSpacer(1);
 	hbox2_StatsB->Add(statsbarTxt3, 1, wxALIGN_CENTER);
 	hbox2_StatsB->Add(statsbarTxt4, 1, wxALIGN_CENTER);
 	statsSizer->Add(hbox1_StatsB, 1, wxEXPAND);
 	statsSizer->Add(hbox2_StatsB, 1, wxEXPAND);
 	statsbar1->SetSizer(statsSizer);
-	//END SECTION
+	//End of status bar section
 
 	//##//End-game dialog section start//
 	endGameDial = new wxDialog(this, 7000, wxString("Game Ended"), wxDefaultPosition, wxSize(450, 300));//add parameters
 	
+	//Defining sizers for use
 	vboxEndGame = new wxBoxSizer(wxVERTICAL);
 	hbox1_EndGame = new wxBoxSizer(wxHORIZONTAL);
 	wxBoxSizer *hbox2_EndGame = new wxBoxSizer(wxHORIZONTAL);
 	wxBoxSizer *hbox3_EndGame = new wxBoxSizer(wxHORIZONTAL);
+	hboxExtra_EndGame = new wxBoxSizer(wxHORIZONTAL);
 
+	//Set up static bitmap controls for end game dial
 	EndGBmp_TL = new wxStaticBitmap(endGameDial, wxID_ANY, *bmpEndGDial_TL40p, wxDefaultPosition); //DEBUG//come here//Try using an empty constructor, since the bmp will be reinitialized later anyways..
 	EndGBmp_TR = new wxStaticBitmap(endGameDial, wxID_ANY, *bmpEndGDial_TR40p, wxDefaultPosition); //DEBUG//come here//Try using an empty constructor, since the bmp will be reinitialized later anyways..
-	
+	EndGBmpMultiplier1 = new wxStaticBitmap(endGameDial, wxID_ANY, *bmpMultiplierX1_5_55p);
+
+	//Set up static text controls for end game dial
 	EndGTxt1 = new wxStaticText(endGameDial, wxID_ANY, wxString("Booom! Wrong Step!"), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER_HORIZONTAL);
 	wxFont fontForEndGTxt1(wxFontInfo(9).Family(wxFONTFAMILY_SWISS).Bold());
 	EndGTxt1->SetFont(fontForEndGTxt1);
@@ -118,16 +114,6 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxP
 	wxFont fontForEndGTxt2(wxFontInfo(16).Italic().Bold().Family(wxFONTFAMILY_SWISS));
 	EndGTxt2->SetFont(fontForEndGTxt2);
 
-	wxBitmapButton *EndGBtn1 = new wxBitmapButton(endGameDial, 7001, *bmpSettings25p, wxDefaultPosition, wxSize(40,40));
-	wxBitmapButton *EndGBtn2 = new wxBitmapButton(endGameDial, 7002, *bmpRestart25p, wxDefaultPosition, wxSize(40,40));
-	wxBitmapButton *EndGBtn3 = new wxBitmapButton(endGameDial, 7003, *bmpExit25p, wxDefaultPosition, wxSize(40,40));
-	EndGBtn1->SetToolTip("Settings");
-	EndGBtn2->SetToolTip("Restart");
-	EndGBtn3->SetToolTip("Quit");
-
-	hboxExtra_EndGame = new wxBoxSizer(wxHORIZONTAL);
-	EndGBmpMultiplier1 = new wxStaticBitmap(endGameDial, wxID_ANY, *bmpMultiplierX1_5_55p);
-	
 	EndGTxt3 = new wxStaticText(endGameDial, wxID_ANY, wxString("Base score: "), wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER_HORIZONTAL);
 	wxFont fontForEndGTxt3(wxFontInfo(11));
 	EndGTxt3->SetFont(fontForEndGTxt3);
@@ -136,33 +122,42 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxP
 	wxFont fontForEndGTxt4(wxFontInfo(10).Bold().Family(wxFONTFAMILY_SWISS));
 	EndGTxt4->SetFont(fontForEndGTxt4);
 
-	hboxExtra_EndGame->Add(EndGTxt3, 1, wxALIGN_CENTER | wxLEFT, 25);
-	hboxExtra_EndGame->Add(EndGBmpMultiplier1, 0, wxALIGN_CENTER | wxLEFT, 15);
-	hboxExtra_EndGame->Add(EndGTxt4, 1, wxALIGN_CENTER | wxLEFT, 5);
+	//Set up bitmap buttons for end game dial
+	wxBitmapButton *EndGBtn1 = new wxBitmapButton(endGameDial, 7001, *bmpSettings25p, wxDefaultPosition, wxSize(40,40));
+	wxBitmapButton *EndGBtn2 = new wxBitmapButton(endGameDial, 7002, *bmpRestart25p, wxDefaultPosition, wxSize(40,40));
+	wxBitmapButton *EndGBtn3 = new wxBitmapButton(endGameDial, 7003, *bmpExit25p, wxDefaultPosition, wxSize(40,40));
+	EndGBtn1->SetToolTip("Settings");
+	EndGBtn2->SetToolTip("Restart");
+	EndGBtn3->SetToolTip("Quit");
 
+	//Fit dialog elements to sizers of end game dial
 	hbox1_EndGame->Add(EndGBmp_TL, 1, wxALIGN_CENTER | wxRIGHT, 3); 
 	hbox1_EndGame->Add(EndGTxt1, 1, wxALIGN_CENTER | wxLEFT | wxRIGHT, 3); 
 	hbox1_EndGame->Add(EndGBmp_TR, 1, wxALIGN_CENTER | wxLEFT, 3); 
 	hbox2_EndGame->Add(EndGTxt2, 1, wxALIGN_CENTER);
+	hboxExtra_EndGame->Add(EndGTxt3, 1, wxALIGN_CENTER | wxLEFT, 25);
+	hboxExtra_EndGame->Add(EndGBmpMultiplier1, 0, wxALIGN_CENTER | wxLEFT, 15);
+	hboxExtra_EndGame->Add(EndGTxt4, 1, wxALIGN_CENTER | wxLEFT, 5);
 	hbox3_EndGame->Add(EndGBtn1, 0, wxALIGN_CENTER | wxRIGHT, 10);
 	hbox3_EndGame->Add(EndGBtn2, 0, wxALIGN_CENTER | wxLEFT | wxRIGHT, 10);
 	hbox3_EndGame->Add(EndGBtn3, 0, wxALIGN_CENTER | wxLEFT, 10);
 	vboxEndGame->Add(hbox1_EndGame, 1, wxALIGN_CENTER);
 	vboxEndGame->Add(hbox2_EndGame, 1, wxALIGN_CENTER);
-	//add extra hbox
-	vboxEndGame->Add(hboxExtra_EndGame, 1, wxALIGN_CENTER);
+	vboxEndGame->Add(hboxExtra_EndGame, 1, wxALIGN_CENTER); //add extra hbox
 	vboxEndGame->Add(hbox3_EndGame, 1, wxALIGN_CENTER);
 	endGameDial->SetSizer(vboxEndGame);
 	vboxEndGame->Layout();
-	//End-game dialog section end//
+	//End game dialog section end//
 
 	//##//Settings dialog section start//
 	SettingsDial = new wxDialog(this, 7050, wxString("Settings"),wxDefaultPosition, wxSize(450, 300));
+	
+	//Define sizers for use by settings dial
 	wxBoxSizer *vboxSettings = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer *hbox1_Settings = new wxBoxSizer(wxHORIZONTAL);
-	//wxBoxSizer *hbox2_Settings = new wxBoxSizer(wxHORIZONTAL);
-	//wxBoxSizer *hbox3_Settings = new wxBoxSizer(wxHORIZONTAL);
 	wxSizer *hbox4_Settings = SettingsDial->CreateButtonSizer(wxCANCEL + wxOK);
+
+	//Set up static text controls and slider control for settings dial
 	wxStaticText *SettingsTxt1 = new wxStaticText(SettingsDial, wxID_ANY, wxString("Game Difficulty"));
 	SettingsTxtDescr = new wxStaticText(SettingsDial, wxID_ANY, wxString("Hello Detective!\nSelect a game difficulty to get started."));
 	wxStaticText *SettingsTxtEZ = new wxStaticText(SettingsDial, wxID_ANY, wxString("Eazy"), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
@@ -171,8 +166,8 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxP
 	SettingsSlider = new wxSlider(SettingsDial, 7051, 2, 1, 3, wxDefaultPosition, wxDefaultSize, wxSL_BOTH);
 	SettingsSlider->SetTickFreq(1);
 	SettingsTxt1->SetFont(SettingsTxt1->GetFont().MakeBold().MakeLarger());
-	//SettingsTxtDescr->SetFont(SettingsTxtDescr->GetFont().MakeLarger());
 
+	//Fit dialog elements to sizers of settings dial
 	hbox1_Settings->Add(SettingsTxtEZ, 1, wxLEFT, 10);
 	hbox1_Settings->AddStretchSpacer(1);
 	hbox1_Settings->Add(SettingsTxtMED, 1);
@@ -205,8 +200,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "OneLoneCoder.com - wxWidgets!", wxP
 	SettingsDial->Bind(wxEVT_BUTTON, &cMain::OnClickSettingsCancel, this, wxID_CANCEL);
 	//Event binding section end//
 
-	int xyz = SettingsDial->ShowModal();
-	
+	int xyz = SettingsDial->ShowModal(); //Show welcome-settings dialog
 }
 
 cMain::~cMain() {
